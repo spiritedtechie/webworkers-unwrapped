93a02b659cd65174f4ed09460100461134ef3a87
Salmen Hichri <salmen.hichri@gmail.com>
2017-05-17 17:01:07 +0100

    Added file selection

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,22 @@
 import { applyFilter } from './filter';
 
-applyFilter();
+const handleFileSelect = evt => {
+
+  let output = [];
+  const files = evt.target.files;
+
+  for(let i = 0; i < files.length; i++) {
+    let file = files[i];
+
+    output.push('<li><strong>', escape(file.name), '</strong> (', file.type || 'n/a', ') - ',
+                file.size, ' bytes, last modified: ',
+                file.lastModifiedDate ? file.lastModifiedDate.toLocaleDateString() : 'n/a',
+                '</li>');
+  };
+
+  document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  document.getElementById('images-input').addEventListener('change', handleFileSelect, false);
+});
